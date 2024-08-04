757a24973ad80e922e961394a54c411ea8b2765d
vjp-luismarianoPO <114507209+vjp-luismarianoPO@users.noreply.github.com>
2024-08-04 21:46:44 +0200

    First commit and configuration

---
+++ glasstrack/src/main/java/com/project/glasstrack/GlasstrackApplication.java
@@ -0,0 +1,13 @@
+package com.project.glasstrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlasstrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlasstrackApplication.class, args);
+	}
+
+}
---
+++ glasstrack/src/test/java/com/project/glasstrack/GlasstrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.glasstrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlasstrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
